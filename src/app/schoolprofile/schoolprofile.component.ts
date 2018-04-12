a99b98dd726743a236efd45e1dea5a27f40232d5
Raj Bhatt <rjbhatt110@gmail.com>
2018-04-12 18:56:57 +0530

    User Editmode

--- src/app/schoolprofile/schoolprofile.component.ts
+++ src/app/schoolprofile/schoolprofile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import {AngularFirestore, AngularFirestoreCollection} from "angularfire2/firestore";
-import {Observable} from "rxjs/Observable";
+import { AngularFirestore, AngularFirestoreCollection } from "angularfire2/firestore";
+import { Observable } from "rxjs/Observable";
 
 interface Post {
   title: string;
@@ -12,20 +12,29 @@ interface Post {
   templateUrl: "./schoolprofile.component.html",
 })
 export class SchoolprofileComponent implements OnInit {
+  pageMode: string;
   content: string;
   title: string;
   postsCol: AngularFirestoreCollection<Post>;
   posts: Observable<Post[]>;
+  mode = 0;
 
   constructor(private afs: AngularFirestore) {
 
   }
-ngOnInit() {
+  ngOnInit() {
     this.postsCol = this.afs.collection("posts");
     this.posts = this.postsCol.valueChanges();
+    this.pageMode = "viewmode";
   }
 
-addPost() {
-    this.afs.collection("posts").add({title: this.title, content: this.content});
+  addPost() {
+    this.afs.collection("posts").add({ title: this.title, content: this.content });
+    this.mode = 0;
+    // this.afs.collection("posts").doc('a').set({});
+  }
+
+  swichmode() {
+    this.mode = 1;
   }
 }
